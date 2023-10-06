21feacb51fafeea02f4d25b2fa08bda3cc9dbdc3
Zenny Chen <zenny_chen@sina.com>
2023-10-07 00:59:49 +0800

    Add files via upload

--- Direct3D12_BasicRendering/Direct3D12_BasicRendering/shaders/basic.frag.hlsl
+++ Direct3D12_BasicRendering/Direct3D12_BasicRendering/shaders/basic.frag.hlsl
@@ -1,11 +1,11 @@
 struct PSInput
 {
-	float4 position : SV_POSITION;
-	float4 color : COLOR;
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
 };
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-	return input.color;
+    return input.color;
 }
 
